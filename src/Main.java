09097b948ae4f792a476ac22b10b3974ba341291
jordonjones <jordon01jones@gmail.com>
2025-12-03 14:02:40 -0600

    exercise6

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
 public class Main {
+    static int books;
+    static double gpa;
+    static long studentID;
+
+    public static void main(String[] args) {
+        books = 65;
+        gpa = 3.7;
+        studentID = 2576164;
+
+        System.out.println("i have " + books + " books " + "my gpa is " + gpa + " " + "my student id is " + studentID);
+    }
 
 }
